7f8abdb026fd78be6a5e166e704562463b31b4fa
Ryan Quinn <quinn.rm@gmail.com>
2019-03-03 18:25:28 -0600

    Syncing
     Changes to be committed:
    	modified:   main.go

--- main.go
+++ main.go
@@ -3,28 +3,50 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"flag"
+	"log"
 	_ "github.com/go-sql-driver/mysql"
 )
 
-func main() {
-	// Create database handle and confirm driver is present.
-	user := "user"
-	pass := "pass"
-	system := "system"
-	database := "database"
-	// DSN format "<user>:<pass>@tcp(<system>)/<database>"
-	DSN := fmt.Sprintf("%s:%s@tcp(%s)/%s", user, pass, system, database)
+var user string
+var pass string
+var server string
+var database string
+var port int
 
-	db, err := sql.Open("mysql", DSN)
+func init() {
+	flag.StringVar(&user, "u", "", "User account for the database.")
+	flag.StringVar(&user, "user", "", "User account for the database. (long)")
+	flag.StringVar(&pass, "p", "", "Password for the database.")
+	flag.StringVar(&pass, "pass", "", "Password for the database. (long)")
+	flag.StringVar(&server, "s", "", "Server for the databse.")
+	flag.StringVar(&server, "server", "127.0.0.1", "Server for the databse. (long)")
+	flag.StringVar(&database, "d", "", "Database on the database server.")
+	flag.StringVar(&database, "database", "", "Database on the database server. (long)")
+
+	flag.IntVar(&port, "P", 3306, "Port to connect to.")
+	flag.IntVar(&port, "port", 3306, "Port to connect to. (long)")
+}
+
+func main() {
+	flag.Parse()
+
+	// Create database handle and confirm driver is present.
+	// DSN format "<user>:<pass>@tcp(<server>)/<database>"
+	dataSource := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s", user, pass, server, port, database)
+
+	db, err := sql.Open("mysql", dataSource)
 	if err != nil {
-		fmt.Println(err)
+		log.Fatal(err)
 	}
 	defer db.Close()
 
 	// Connect and check the server version
 	var version string
 	db.QueryRow("SELECT VERSION()").Scan(&version)
-	fmt.Println("Connected to:", version)
+	fmt.Printf("DSN: tcp://%s@%s:%d/%s Version: %s\n", user, server, port, database, version)
+
+	db.Query
 }
 
 // References: https://mariadb.com/resources/blog/using-go-with-mariadb
