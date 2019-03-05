dc83d06d2cf29fcb4795980ed80898e820da3e22
Ryan Quinn <rquinn@op5.com>
2019-03-04 18:25:16 -0600

    Checkpoint. Deleting host complete. Working on adding entries.
     Changes to be committed:
    	modified:   main.go

--- main.go
+++ main.go
@@ -12,9 +12,10 @@ import (
 
 var user string
 var pass string
-var server string
-var database string
-var port int
+var server1 string
+var server2 string
+var database1 string
+var database2 string
 
 type DbRow struct {
 	Timestamp          uint
@@ -36,58 +37,128 @@ func init() {
 	flag.StringVar(&user, "user", "", "User account for the database. (long)")
 	flag.StringVar(&pass, "p", "", "Password for the database.")
 	flag.StringVar(&pass, "pass", "", "Password for the database. (long)")
-	flag.StringVar(&server, "s", "", "Server for the databse.")
-	flag.StringVar(&server, "server", "127.0.0.1", "Server for the databse. (long)")
-	flag.StringVar(&database, "d", "", "Database on the database server.")
-	flag.StringVar(&database, "database", "", "Database on the database server. (long)")
-	flag.IntVar(&port, "P", 3306, "Port to connect to.")
-	flag.IntVar(&port, "port", 3306, "Port to connect to. (long)")
+	flag.StringVar(&server1, "s1", "127.0.0.1", "Source server for the databse. server:port#")
+	flag.StringVar(&server1, "server1", "127.0.0.1", "Source server for the databse. server:port# (long)")
+	flag.StringVar(&server2, "s2", "127.0.0.1", "Destination Server for the databse. server:port#")
+	flag.StringVar(&server2, "server2", "127.0.0.1", "Destination server for the databse. server:port# (long)")
+	flag.StringVar(&database1, "d1", "", "Database on the servers.")
+	flag.StringVar(&database1, "database1", "", "Database on the servers. (long)")
+	flag.StringVar(&database2, "d2", "", "Database on the servers.")
+	flag.StringVar(&database2, "database2", "", "Database on the servers. (long)")
 }
 
 func main() {
 	flag.Parse()
 
-	//var results DbRow
 	var hostnames []string
 
 	// Create database handle and confirm driver is present.
 	// DSN format "<user>:<pass>@tcp(<server>)/<database>"
-	dataSource := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s", user, pass, server, port, database)
+	dataSource := fmt.Sprintf("%s:%s@tcp(%s)/%s", user, pass, server1, database1)
+	dataDestination := fmt.Sprintf("%s:%s@tcp(%s)/%s", user, pass, server2, database2)
 
-	db, err := sql.Open("mysql", dataSource)
+	db1, err := sql.Open("mysql", dataSource)
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer db.Close()
+	defer db1.Close()
 
-	// Connect and check the server version
-	var version string
-	db.QueryRow("SELECT VERSION()").Scan(&version)
-	fmt.Printf("DSN: tcp://%s@%s:%d/%s Version: %s\n", user, server, port, database, version)
+	err = db1.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	rows, err := db.Query("select host_name from report_data")
+	db2, err := sql.Open("mysql", dataDestination)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db2.Close()
+
+	err = db2.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Query server1 to get the rows.
+	rows, err := db1.Query("select distinct host_name from report_data where host_name != '' order by host_name")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer rows.Close()
 
 	for rows.Next() {
-		var result string
-		err := rows.Scan(&result)
+		var host string
+		err := rows.Scan(&host)
 		if err != nil {
 			log.Fatal(err)
 		}
-		hostnames = append(hostnames, result)
+		hostnames = append(hostnames, host)
 	}
 
-	fmt.Println(strings.Join(hostnames, ","))
+	log.Printf("Hosts in poller: %s\n", strings.Join(hostnames, ", "))
 
 	err = rows.Err()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	//rows, err := db.Query("delete from report_data where host_name like(?)", strings.Join(hostnames, ","))
+	tx, err := db2.Begin()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer tx.Rollback()
+
+	stmt, err := tx.Prepare("delete from report_data where host_name = ?")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer stmt.Close()
+
+	for _, host := range hostnames {
+		fmt.Printf("Deleting host %s...\t", host)
+		results, err := stmt.Exec(host)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		rowsAffected, err := results.RowsAffected()
+		if err != nil {
+			log.Printf("Error with rows affecteed, %s\n", err)
+		}
+		log.Printf("%d rows affected.\n", rowsAffected)
+	}
+
+	err = stmt.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = tx.Commit()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, host := range hostnames {
+		fmt.Println(host)
+		rows, err := db1.Query("select timestamp, event_type, flags, attrib, host_name, service_description, state, hard, retry, downtime_depth, output, long_output from report_data where host_name = ?", host)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer rows.Close()
+
+		for rows.Next() {
+			var timestamp, event_type, attrib, state, hard, retry, downtime_depth uint64
+			var flags, host_name, service_description, output, long_output string
+			err = rows.Scan(&timestamp, &event_type, &flags, &attrib, &host_name, &service_description, &state, &hard, &retry, &downtime_depth, &output, &long_output)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			fmt.Printf("%d %d %d %s %s %d %d %d %d %s %s", timestamp, event_type, flags, attrib, host_name, service_description, state, hard, retry, downtime_depth, output, long_output)
+		}
+	}
 }
 
-// References: https://mariadb.com/resources/blog/using-go-with-mariadb
+// References:
+//   https://mariadb.com/resources/blog/using-go-with-mariadb
+//   http://go-database-sql.org
