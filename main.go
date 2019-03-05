2d423da42369a3ba266ecca70805a5041a052c49
Ryan Quinn <quinn.rm@gmail.com>
2019-03-04 23:20:37 -0600

    SQL portion is complete.
    Entries are deleted from the master database then readded from the
    poller.
    
     Changes to be committed:
    	modified:   main.go

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import (
 	"flag"
 	"fmt"
 	"log"
-	"strings"
+	"time"
 
 	_ "github.com/go-sql-driver/mysql"
 )
@@ -16,6 +16,9 @@ var server1 string
 var server2 string
 var database1 string
 var database2 string
+var skipdelete bool
+var skipinsert bool
+var noop bool
 
 type DbRow struct {
 	Timestamp          uint
@@ -45,6 +48,10 @@ func init() {
 	flag.StringVar(&database1, "database1", "", "Database on the servers. (long)")
 	flag.StringVar(&database2, "d2", "", "Database on the servers.")
 	flag.StringVar(&database2, "database2", "", "Database on the servers. (long)")
+
+	flag.BoolVar(&skipdelete, "skip-delete", false, "Skip deleting rows. (Not implemented)")
+	flag.BoolVar(&skipinsert, "skip-insert", false, "Skip inserting rows. (Not implemeted)")
+	flag.BoolVar(&noop, "nop", false, "No operations. (Not implemented)")
 }
 
 func main() {
@@ -59,30 +66,28 @@ func main() {
 
 	db1, err := sql.Open("mysql", dataSource)
 	if err != nil {
-		log.Fatal(err)
+		log.Panic(err)
 	}
 	defer db1.Close()
 
-	err = db1.Ping()
-	if err != nil {
-		log.Fatal(err)
+	if err = db1.Ping(); err != nil {
+		log.Panic(err)
 	}
 
 	db2, err := sql.Open("mysql", dataDestination)
 	if err != nil {
-		log.Fatal(err)
+		log.Panic(err)
 	}
 	defer db2.Close()
 
-	err = db2.Ping()
-	if err != nil {
-		log.Fatal(err)
+	if err = db2.Ping(); err != nil {
+		log.Panic(err)
 	}
 
 	// Query server1 to get the rows.
 	rows, err := db1.Query("select distinct host_name from report_data where host_name != '' order by host_name")
 	if err != nil {
-		log.Fatal(err)
+		log.Panic(err)
 	}
 	defer rows.Close()
 
@@ -90,73 +95,113 @@ func main() {
 		var host string
 		err := rows.Scan(&host)
 		if err != nil {
-			log.Fatal(err)
+			log.Panic(err)
 		}
 		hostnames = append(hostnames, host)
 	}
 
-	log.Printf("Hosts in poller: %s\n", strings.Join(hostnames, ", "))
-
-	err = rows.Err()
-	if err != nil {
-		log.Fatal(err)
+	if err = rows.Err(); err != nil {
+		log.Panic(err)
 	}
 
 	tx, err := db2.Begin()
 	if err != nil {
-		log.Fatal(err)
+		log.Panic(err)
 	}
 	defer tx.Rollback()
 
 	stmt, err := tx.Prepare("delete from report_data where host_name = ?")
 	if err != nil {
-		log.Fatal(err)
+		log.Panic(err)
 	}
 	defer stmt.Close()
 
 	for _, host := range hostnames {
-		fmt.Printf("Deleting host %s...\t", host)
+		fmt.Printf("Deleting %s...\t", host)
+
 		results, err := stmt.Exec(host)
 		if err != nil {
-			log.Fatal(err)
+			log.Panic(err)
 		}
 
 		rowsAffected, err := results.RowsAffected()
 		if err != nil {
-			log.Printf("Error with rows affecteed, %s\n", err)
+			log.Panic(err)
 		}
-		log.Printf("%d rows affected.\n", rowsAffected)
+		fmt.Printf("%d rows deleted.\n", rowsAffected)
 	}
 
-	err = stmt.Close()
-	if err != nil {
-		log.Fatal(err)
+	if err = stmt.Close(); err != nil {
+		log.Panic(err)
 	}
 
-	err = tx.Commit()
-	if err != nil {
-		log.Fatal(err)
+	if err = tx.Commit(); err != nil {
+		log.Panic(err)
 	}
 
 	for _, host := range hostnames {
-		fmt.Println(host)
+		var rowsInserted int64
+
+		fmt.Printf("Adding %s...\t", host)
+
+		tx, err = db2.Begin()
+		if err != nil {
+			log.Panic(err)
+		}
+		defer tx.Rollback()
+
+		stmt, err := tx.Prepare("insert into report_data(timestamp, event_type, flags, attrib, host_name, service_description, state, hard, retry, downtime_depth, output, long_output) values(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)")
+		if err != nil {
+			log.Panic(err)
+		}
+		defer stmt.Close()
+
+		// Move to a function and use a goroutine to increate consurrency.
 		rows, err := db1.Query("select timestamp, event_type, flags, attrib, host_name, service_description, state, hard, retry, downtime_depth, output, long_output from report_data where host_name = ?", host)
 		if err != nil {
-			log.Fatal(err)
+			log.Panic(err)
 		}
+
 		defer rows.Close()
 
 		for rows.Next() {
-			var timestamp, event_type, attrib, state, hard, retry, downtime_depth uint64
-			var flags, host_name, service_description, output, long_output string
+			var flags, attrib, downtime_depth sql.NullInt64
+			var output, long_output sql.NullString
+			var timestamp, event_type, state, hard, retry int64
+			var host_name, service_description string
+
 			err = rows.Scan(&timestamp, &event_type, &flags, &attrib, &host_name, &service_description, &state, &hard, &retry, &downtime_depth, &output, &long_output)
 			if err != nil {
-				log.Fatal(err)
+				log.Panic(err)
 			}
 
-			fmt.Printf("%d %d %d %s %s %d %d %d %d %s %s", timestamp, event_type, flags, attrib, host_name, service_description, state, hard, retry, downtime_depth, output, long_output)
+			//fmt.Printf("%d %d %d %s %s %d %d %d %d %s %s\n", timestamp, event_type, flags, attrib, host_name, service_description, state, hard, retry, downtime_depth, output, long_output)
+			results, err := stmt.Exec(timestamp, event_type, flags, attrib, host_name, service_description, state, hard, retry, downtime_depth, output, long_output)
+			rowsAffected, err := results.RowsAffected()
+			if err != nil {
+				log.Panic(err)
+			}
+
+			rowsInserted += rowsAffected
+		}
+
+		fmt.Printf("%d rows inserted\n", rowsInserted)
+		rowsInserted = 0
+
+		if err = rows.Err(); err != nil {
+			log.Panic(err)
+		}
+
+		if err = stmt.Close(); err != nil {
+			log.Panic(err)
+		}
+
+		if err = tx.Commit(); err != nil {
+			log.Panic(err)
 		}
 	}
+
+	fmt.Println("Work finished", time.Now())
 }
 
 // References:
